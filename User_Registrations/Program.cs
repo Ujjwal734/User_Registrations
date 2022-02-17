4d92ea465cc7c599a3295d4989b0dcc48f258259
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2022-02-17 21:38:50 +0530

    Validating Last Name

--- User_Registrations/Program.cs
+++ User_Registrations/Program.cs
@@ -1,4 +1,18 @@
 ﻿using User_Registrations;
 Console.WriteLine("Hello, Welcome to User Registration Programe!");
+Console.WriteLine("Choose Option what you want to validate :- ");
+Console.WriteLine("1. First Name \n2.Last Name");
 UserRegistration userregistration = new UserRegistration();
-userregistration.FirstName();
+int option = Convert.ToInt32(Console.ReadLine());
+switch(option)
+{
+    case 1:
+        userregistration.FirstName();
+        break;
+    case 2:
+        userregistration.LastName();
+        break;
+    default:
+        Console.WriteLine("Choose From available Option :) ");
+        break;
+}
--- User_Registrations/UserRegistration.cs
+++ User_Registrations/UserRegistration.cs
@@ -11,11 +11,19 @@ namespace User_Registrations
     {
         public void FirstName()
         {
-             Console.WriteLine("Enter your name :- ");
+             Console.WriteLine("Enter your First Name :- ");
              string instr = Console.ReadLine();
              const string name = @"^[A-Z]{1}[a-z]{3,9}$";
              Regex rg = new Regex(name);
              Console.WriteLine(rg.IsMatch(instr));
+        }
+        public void LastName()
+        {
+            Console.WriteLine("Enter your Last Name :- ");
+            string instr = Console.ReadLine();
+            const string name = @"^[A-Z]{1}[a-z]{3,15}$";
+            Regex rg = new Regex(name);
+            Console.WriteLine(rg.IsMatch(instr));
         }
     }
 }
